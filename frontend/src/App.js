5ec1af34fbe9253c0f0ffe8b3af4275a6e14ecbf
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2025-10-20 20:12:05 +0530

    Set up basic React app with routing and main pages

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile/:id" element={<Profile />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
