55ca651d48ea35c1957646d6849f8a1cc00301e8
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2025-10-20 20:26:38 +0530

    Added post creation form and display posts using Redux

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addPost, setPosts } from "../redux/postSlice";
 import Post from "../components/Post";
 
 const Home = () => {
-  const posts = [
-    { content: "Hello World!", likes: [] },
-    { content: "My first post!", likes: [] },
-  ];
+  const [content, setContent] = useState("");
+  const posts = useSelector((state) => state.post.posts);
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newPost = { content, likes: [] };
+    dispatch(addPost(newPost));
+    setContent("");
+  };
 
   return (
     <div>
       <h2>Home Page</h2>
-      {posts.map((post, index) => (
-        <Post key={index} post={post} />
-      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="What's on your mind?"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <button type="submit">Post</button>
+      </form>
+      <div>
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
     </div>
   );
 };
