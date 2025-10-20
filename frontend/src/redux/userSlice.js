044b80c8f4b0163d8e3af2c8bfe70b30bd9ded2a
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2025-10-20 20:14:38 +0530

    Setup Redux store with user and post slices

---
+++ frontend/src/redux/userSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  currentUser: null,
+  isAuthenticated: false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    loginSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.isAuthenticated = true;
+    },
+    logout: (state) => {
+      state.currentUser = null;
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const { loginSuccess, logout } = userSlice.actions;
+export default userSlice.reducer;
