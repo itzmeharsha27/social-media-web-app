acf64d2d66880294e88cc8d31addcdf85e31a273
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2025-10-20 20:18:02 +0530

    Connected Navbar to App and added placeholder posts on Home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,12 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Post from "../components/Post";
 
 const Home = () => {
-  return <div>Home Page - Posts will appear here</div>;
+  const posts = [
+    { content: "Hello World!", likes: [] },
+    { content: "My first post!", likes: [] },
+  ];
+
+  return (
+    <div>
+      <h2>Home Page</h2>
+      {posts.map((post, index) => (
+        <Post key={index} post={post} />
+      ))}
+    </div>
+  );
 };
 
 export default Home;
